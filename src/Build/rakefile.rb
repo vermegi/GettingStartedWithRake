b1a1050fb323e1d04b0be6c1e61c5b1edcf25f60
Gitte <gitte.vermeiren@gmail.com>
2012-07-01 21:59:26 +0200

    Added publish of the web site.

--- src/Build/rakefile.rb
+++ src/Build/rakefile.rb
@@ -1,13 +1,26 @@
 require 'albacore'
 
+@SOLUTIONFOLDER = "..\\A.Simple.App"
 @SOLUTION = "..\\A.Simple.App\\A.Simple.App.sln"
+@PROJECTFOLDER = "..\\A.Simple.App\\A.Simple.App"
 @CONFIG = ENV['CONFIG'] || 'Debug'
 
 desc "the default task"
-task :default => [:buildIt]
+task :default => [:buildIt, :publish]
 
 msbuild :buildIt do |msb|
 	msb.properties :configuration => @CONFIG
 	msb.targets :Clean, :Build
 	msb.solution = @SOLUTION
+end
+
+desc "Publish the web site"
+msbuild :publish do |msb|
+  msb.properties = {:configuration=>@CONFIG}
+  msb.targets [:ResolveReferences,:_CopyWebApplication]
+  msb.properties = {
+  :webprojectoutputdir=>"../../build/builds/#{@CONFIG}",
+  :outdir => "../../build/builds/#{@CONFIG}"
+  }
+  msb.solution = "#{@PROJECTFOLDER}/A.Simple.App.csproj"
 end
