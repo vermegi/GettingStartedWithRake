f4aab15b33834dd99724d38ea7b45ff24904e487
Gitte <gitte.vermeiren@gmail.com>
2012-07-01 15:07:33 +0200

    A better build task

--- src/Build/rakefile.rb
+++ src/Build/rakefile.rb
@@ -1,10 +1,13 @@
+require 'albacore'
+
 @SOLUTION = "..\\A.Simple.App\\A.Simple.App.sln"
-@DOT_NET_PATH = 'C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\'
 @CONFIG = 'Debug'
 
 desc "the default task"
 task :default => [:buildIt]
 
-task :buildIt do
-	sh "#{@DOT_NET_PATH}msbuild.exe /p:Configuration=#{@CONFIG} #{@SOLUTION}"
+msbuild :buildIt do |msb|
+	msb.properties :configuration => @CONFIG
+	msb.targets :Clean, :Build
+	msb.solution = @SOLUTION
 end
